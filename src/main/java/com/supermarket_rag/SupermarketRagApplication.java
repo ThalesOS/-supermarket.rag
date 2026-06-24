c5a7c3caf6d4dd73e510fc887c768943579f6904
Thales Oliveira <thales.oliveira2567@gmail.com>
2026-06-24 13:51:46 -0300

    first commit

---
+++ src/main/java/com/supermarket_rag/SupermarketRagApplication.java
@@ -0,0 +1,13 @@
+package com.supermarket_rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermarketRagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/supermarket_rag/SupermarketRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supermarket_rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
