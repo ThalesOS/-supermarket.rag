406003adbc4736d87711ade679f980e17a9c792e
Thales Oliveira <thales.oliveira2567@gmail.com>
2026-06-24 18:02:29 -0300

    Implementação das interfaces repository

---
+++ src/main/java/com/supermarket_rag/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.supermarket_rag.repositories;
+
+import com.supermarket_rag.models.Pedido;
+import com.supermarket_rag.models.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
