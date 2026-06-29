d7e13f59a28d8eb5ce57ac8ea63ef068598477d5
Thales Oliveira <thales.oliveira2567@gmail.com>
2026-06-29 00:54:22 -0300

    Implementaação de ProdutoService

--- src/main/java/com/supermarket_rag/repositories/ProdutoRepository.java
+++ src/main/java/com/supermarket_rag/repositories/ProdutoRepository.java
@@ -7,4 +7,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    boolean existsByNome(String nome);
+
+    Produto findByName(String nome);
 }
